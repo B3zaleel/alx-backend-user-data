3b63655dde21a8739575d42fa043f2087fa30847
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-16 18:18:42 +0100

    :wrench: Change location of auth scheme setup

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,12 +7,19 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -58,11 +65,4 @@ def authenticate_user():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth_type = getenv('AUTH_TYPE', 'auth')
-    if auth_type == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    if auth_type == 'basic_auth':
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
     app.run(host=host, port=port)
