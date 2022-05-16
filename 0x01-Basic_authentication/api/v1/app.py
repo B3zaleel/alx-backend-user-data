827c84486437e7f9f40103647e66b249052285b1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-16 17:48:50 +0100

    Revert ":wrench: Change AUTH_TYPE's default value"
    This reverts commit 3766a1b5b3bce790892682cc880c816238ef9b6d.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -58,7 +58,7 @@ def authenticate_user():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth_type = getenv('AUTH_TYPE', None)
+    auth_type = getenv('AUTH_TYPE', 'auth')
     if auth_type == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
