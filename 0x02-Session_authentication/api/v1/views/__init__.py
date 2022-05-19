8ec9ca02be8e70980d40b59bc8512d099153d28d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-19 11:07:14 +0100

    :white_check_mark: Complete task 7

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,5 +7,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import *
 
 User.load_from_file()
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+
+import os
+from typing import Tuple
+from flask import abort, jsonify, request
+
+from models.user import User
+from api.v1.views import app_views
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    """POST /api/v1/auth_session/login
+    Return:
+      - JSON representation of a User object.
+    """
+    not_found_res = { "error": "no user found for this email" }
+    email = request.form.get('email')
+    if email is None or len(email.strip()) == 0:
+        return jsonify({ "error": "email missing" }), 400
+    password = request.form.get('password')
+    if password is None or len(password.strip()) == 0:
+        return jsonify({ "error": "password missing" }), 400
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify(not_found_res), 404
+    if len(users) <= 0:
+        return jsonify(not_found_res), 404
+    if users[0].is_valid_password(password):
+        from api.v1.app import auth
+        sessiond_id = auth.create_session(getattr(users[0], 'id'))
+        res = jsonify(users[0].to_json())
+        res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+        return res
+    return jsonify({ "error": "wrong password" }), 401
