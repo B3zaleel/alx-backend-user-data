4c2a86d142272884329425157baaf15f45d9f775
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-19 15:31:12 +0100

    :wrench: Refactor script

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -2,7 +2,6 @@
 """Session authentication with expiration module for the API.
 """
 import os
-import re
 from flask import request
 from datetime import datetime, timedelta
 
@@ -17,17 +16,16 @@ class SessionExpAuth(SessionAuth):
         """Initializes a new SessionExpAuth instance.
         """
         super().__init__()
-        tmp = os.getenv('SESSION_DURATION', '0')
-        if re.fullmatch(r'[+-]?\d+', tmp) is not None:
-            self.session_duration = int(tmp)
-        else:
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION', '0'))
+        except Exception:
             self.session_duration = 0
 
     def create_session(self, user_id=None):
         """Creates a session id for the user.
         """
         session_id = super().create_session(user_id)
-        if session_id is None:
+        if type(session_id) != str:
             return None
         self.user_id_by_session_id[session_id] = {
             'user_id': user_id,
@@ -35,13 +33,15 @@ class SessionExpAuth(SessionAuth):
         }
         return session_id
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) -> str:
         """Retrieves the user id of the user associated with
         a given session id.
         """
         if session_id in self.user_id_by_session_id:
             session_dict = self.user_id_by_session_id[session_id]
-            if self.session_duration > 0 or 'created_at' not in session_dict:
+            if self.session_duration <= 0:
+                return session_dict['user_id']
+            if 'created_at' not in session_dict:
                 return None
             cur_time = datetime.now()
             time_span = timedelta(seconds=self.session_duration)
