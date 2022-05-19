1104edb080056a8ee7dce0c9eb7da615811a2609
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-19 08:02:55 +0100

    Fix JSON conversion error

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -31,7 +31,7 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         else:
-            return jsonify(request.current_user)
+            return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
