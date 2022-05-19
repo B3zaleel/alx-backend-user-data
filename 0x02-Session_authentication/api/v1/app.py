0018dc31416155d0feba3b0379c0ec4cdcd248b8
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-19 12:56:23 +0100

    :wrench: Refactor authenticate_user event listener

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,9 +59,8 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
