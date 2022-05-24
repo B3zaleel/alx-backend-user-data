6bdbf72fa75629fc0907b85514a456da68bcfb14
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-24 05:21:19 +0100

    :wrench: Specify length of string columns in User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,7 @@ class User(Base):
     """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
