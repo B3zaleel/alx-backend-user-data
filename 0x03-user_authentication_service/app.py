1b3d55cdb1f82dcc593a5368c284f0347a95a03f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-24 06:40:32 +0100

    :wrench: Remove CORS support

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """A simple Flask app with user authentication features.
 """
-from flask_cors import CORS
 from flask import Flask, jsonify, request, abort, redirect
 
 from auth import Auth
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 AUTH = Auth()
 
 
