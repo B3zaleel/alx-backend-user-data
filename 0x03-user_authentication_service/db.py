7596d306154810901bd49f7c8c22a2652c1cccb9
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-24 05:28:06 +0100

    :wrench: Turn off SQLAlchemy engine's echoing

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
